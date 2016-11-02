41d90864d8665f9073c47782ba894ec76fceeed5
Richard Lam <richardvclam@gmail.com>
2016-11-01 19:21:38 -0700

    Mining Linked Nodes

--- src/assignments/mining/Mining.java
+++ src/assignments/mining/Mining.java
@@ -25,6 +25,48 @@ public class Mining {
 			pit.add(p);
 		}
 		
+		ArrayList<ArrayList<ArrayList<Integer>>> parents = new ArrayList<ArrayList<ArrayList<Integer>>>();
+		
+		for (int i = 1; i < pit.size(); i++) {
+			ArrayList<ArrayList<Integer>> parentRow = new ArrayList<ArrayList<Integer>>();
+			
+			int currentParentNode = 0;	
+			int[] parent = pit.get(i - 1).get(currentParentNode);
+			int currentParentWidth = parent[0];
+			
+			for (int j = 0; j < pit.get(i).size(); j++) {
+				int[] current = pit.get(i).get(j);
+				boolean run = true;
+				
+				ArrayList<Integer> ps = new ArrayList<Integer>();
+				
+				if (currentParentWidth == 0) {
+					currentParentNode++;
+					parent = pit.get(i - 1).get(currentParentNode);
+					currentParentWidth += parent[0];
+				}
+				
+				do {
+					if (currentParentWidth >= current[0]) {
+						currentParentWidth -= current[0];
+						ps.add(currentParentNode);
+						run = false;
+						break;
+					} else if (currentParentWidth < current[0]) {
+						ps.add(currentParentNode);
+						currentParentNode++;
+						parent = pit.get(i - 1).get(currentParentNode);
+						currentParentWidth += parent[0];
+					}
+				} while (run);
+				
+				parentRow.add(ps);
+			}
+			parents.add(parentRow);
+		}
+		
+		
+		
 		PrintWriter writer = new PrintWriter("src/assignments/mining/blocks.txt");
 		
 		writer.close();
