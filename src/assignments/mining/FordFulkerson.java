14b239e8942828e09f8c4962fda163808334835c
Richard Lam <richardvclam@gmail.com>
2016-11-15 17:37:31 -0800

    Mining Update V

--- src/assignments/mining/FordFulkerson.java
+++ src/assignments/mining/FordFulkerson.java
@@ -1,5 +1,7 @@
 package assignments.mining;
 
+import java.util.ArrayList;
+
 public class FordFulkerson {
 	
 	public static void main(String[] args) {
@@ -11,14 +13,40 @@ public class FordFulkerson {
 			                {0, 0, 0, 0, 0, 0}
               };
 		
-		int[][] flow = new int[graph.length][graph[0].length];
+		int[][] residualGraph = new int[graph.length][graph[0].length];
+		for (int i = 0; i < graph.length; i++) {
+			for (int j = 0; j < graph.length; j++) {
+				residualGraph[i][j] = graph[i][j];
+			}
+		}
+		int maxFlow = 0;
+		for (int i = 0; i < 5; i++) {
+			maxFlow += maxFlow(graph, residualGraph, new ArrayList<Integer>(), 0, Integer.MAX_VALUE, maxFlow);
+			//System.out.println("Max flow: " + maxFlow);
+		}
 	}
 	
-	public static int maxFlow(int[][] graph, int[][] flow, int row, int col, int capacity) {
-		int maxFlow = 0;
+	public static int maxFlow(int[][] capacityGraph, int[][] residualGraph, ArrayList<Integer> stack, int row, int capacity, int maxFlow) {
+		if (row == capacityGraph.length - 1) {
+			System.out.println(capacity);
+			return capacity;
+		}
 		
-		for (int i = 0; i < graph[0].length; i++) {
-			
+		for (int i = 0; i < capacityGraph[row].length; i++) {
+			if (i == row || stack.contains(i) || i == 0) {
+				continue;
+			}
+			if (capacityGraph[row][i] > 0 /*&& residualGraph[row][i] < capacityGraph[row][i]*/) {
+				stack.add(i);
+				int flow = maxFlow(capacityGraph, residualGraph, stack, i, capacityGraph[row][i] < capacity ? capacityGraph[row][i] : capacity, maxFlow);
+				stack.remove(stack.size() - 1);
+				//residualGraph[row][i] -= flow;
+				//residualGraph[i][row] += flow;
+				capacityGraph[row][i] -= flow;
+				capacityGraph[i][row] += flow;
+				maxFlow = flow;
+				break;
+			}
 		}
 		
 		return maxFlow;
